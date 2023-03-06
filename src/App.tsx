d9c1bdb9764fd070a55219b619dbe66b90b7b606
Joshua McCorkell <joshua@themccorkells.com>
2023-03-06 14:09:37 +1000

    Changed formatter, no select on checkbox label

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,66 @@
-import InputField from './InputField';
-import CheckBoxField from './CheckBoxField';
-import React, { FC, ChangeEventHandler, useEffect, useState } from 'react';
-import './App.scss';
+import InputField from "./InputField";
+import CheckBoxField from "./CheckBoxField";
+import React, { FC, ChangeEventHandler, useEffect, useState } from "react";
+import "./App.scss";
 
 function useInput(defaultValue: string) {
-    const [value, setValue] = useState(defaultValue);
+  const [value, setValue] = useState(defaultValue);
 
-    const handleChange:ChangeEventHandler<HTMLInputElement> = (e) => {
-        setValue(e.target.value);
-    }
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+  };
 
-    return {
-        value,
-        onChange: handleChange,
-    };
+  return {
+    value,
+    onChange: handleChange,
+  };
 }
 
 function useCheckbox(defaultValue: boolean) {
-    const [checked, setChecked] = useState(defaultValue);
+  const [checked, setChecked] = useState(defaultValue);
 
-    const handleChange:ChangeEventHandler<HTMLInputElement> = (e) => {
-        setChecked(e.target.checked);
-    }
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setChecked(e.target.checked);
+  };
 
-    return {
-        checked,
-        onChange: handleChange,
-    };
+  return {
+    checked,
+    onChange: handleChange,
+  };
 }
 
 const App: FC = () => {
-    const firstName = useInput("Joshua");
-    const lastName = useInput("McCorkell");
-    const favouriteHobby = useInput("Coding");
-    const usesTypeScript = useCheckbox(true);
+  const firstName = useInput("Joshua");
+  const lastName = useInput("McCorkell");
+  const favouriteHobby = useInput("Coding");
+  const usesTypeScript = useCheckbox(true);
 
-    useEffect(() => {
-        document.title = firstName.value + " " + lastName.value;
-    }, [firstName, lastName]);
+  useEffect(() => {
+    document.title = firstName.value + " " + lastName.value;
+  }, [firstName, lastName]);
 
-    return (
-        <div className="app">
-            <div className="input-fields">
-                <InputField
-                    {...firstName}
-                    id="first-name-input"
-                    label="First Name:"
-                />
-                <InputField
-                    {...lastName}
-                    id="last-name-input"
-                    label="Last Name:"
-                />
-                <InputField
-                    {...favouriteHobby}
-                    id="favourite-hobby-input"
-                    label="Favourite Hobby:"
-                />
-                <CheckBoxField
-                    {...usesTypeScript}
-                    id="uses-typescript-checkbox"
-                    label={usesTypeScript.checked? "Uses TypeScript!" : "Doesn't Use TypeScript"}
-                />
-            </div>
-            
-        </div>
-    );
-}
-
+  return (
+    <div className="app">
+      <div className="input-fields">
+        <InputField {...firstName} id="first-name-input" label="First Name:" />
+        <InputField {...lastName} id="last-name-input" label="Last Name:" />
+        <InputField
+          {...favouriteHobby}
+          id="favourite-hobby-input"
+          label="Favourite Hobby:"
+        />
+        <CheckBoxField
+          {...usesTypeScript}
+          id="uses-typescript-checkbox"
+          label={
+            usesTypeScript.checked
+              ? "Uses TypeScript!"
+              : "Doesn't Use TypeScript"
+          }
+        />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/CheckBoxField.tsx
+++ src/CheckBoxField.tsx
@@ -1,30 +1,25 @@
-import { FC, ChangeEventHandler } from 'react';
-
+import { FC, ChangeEventHandler } from "react";
 
 interface CheckboxFieldProps {
-    label: string;
-    checked: boolean;
-    id: string;
-    onChange: ChangeEventHandler<HTMLInputElement>;
+  label: string;
+  checked: boolean;
+  id: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-const CheckBoxField: FC<CheckboxFieldProps> = props => {
-    return (
-        <div className="input-field">
-            <input
-                className="hidden-checkbox"
-                id={props.id}
-                type="checkbox"
-                checked={props.checked}
-                onChange={props.onChange}
-            />
-            <label
-                htmlFor={props.id}
-            >
-                {props.label}
-            </label>
-        </div>
-    );
+const CheckBoxField: FC<CheckboxFieldProps> = (props) => {
+  return (
+    <div className="input-field">
+      <input
+        className="hidden-checkbox"
+        id={props.id}
+        type="checkbox"
+        checked={props.checked}
+        onChange={props.onChange}
+      />
+      <label htmlFor={props.id}>{props.label}</label>
+    </div>
+  );
 };
 
 export default CheckBoxField;
